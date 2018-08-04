c91c313d72402f60bc40b9a59bcb11fdebc42b9a
Sakib <sakibultimate@gmail.com>
2018-08-04 22:38:05 +0600

    updated sel_test

--- sel_test.py
+++ sel_test.py
@@ -9,15 +9,29 @@ from selenium import webdriver
 
 import time
 
-driver= webdriver.Chrome("E:\Anaconda\cdrivers\chromedriver")
+#driver= webdriver.Chrome("E:\Anaconda\cdrivers\chromedriver")
+
+chromeOptions = webdriver.ChromeOptions()
+prefs = {'profile.managed_default_content_settings.images':2,'disk-cache-size': 4096} #images off ans disc cache 4mb
+chromeOptions.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome("E:\Anaconda\cdrivers\chromedriver",chrome_options=chromeOptions) #image off driver
 
 driver.get("http://jobs.bdjobs.com/jobsearch.asp?fcatId=8&icatId=")
-print('waiting for 10 sec')
+#time.sleep(5)
 
-print('done waiting')
+last_nmbr=driver.find_element_by_xpath("//*[@id=\"topPagging\"]/ul/li[7]/a").text  #click counter generation
+last_nmbr=last_nmbr.replace(".","") #extra ... removal
+max_clicks=int(last_nmbr) 
 
-driver.find_element_by_xpath("//*[@id=\"jobList\"]/div[1]/div/div[24]/div[1]/div/div[1]/div/ul/li[2]/a").click()
-#driver.quit()
+i=1
+while(i<=max_clicks):
+    driver.find_element_by_xpath("//*[@id=\"jobList\"]/div[1]/div/div[24]/div[1]/div/div[1]/div/ul/li[2]/a").click()
+    page_source=driver.page_source
+    time.sleep(1)
+    i+=1
+#clicks on the next button
+
+driver.quit()
 
 
 '''
