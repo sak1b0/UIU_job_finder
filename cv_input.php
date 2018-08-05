8851ee1e1a8e8ec64bc6ee90394a58b5f2714838
Sakib <sakibultimate@gmail.com>
2018-08-06 00:36:26 +0600

    cv input form is ok

--- cv_input.php
+++ cv_input.php
@@ -1,161 +1,164 @@
-<?php //include"includes/reg_attempt.php"; ?> 
-<!DOCTYPE html>    
-<html lang="en" >
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>CV information</title>
-  
+    <meta charset="UTF-8">
+    <title>CV information</title>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 
-  <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
     <link rel="stylesheet" href="vendor/bootstrap/css/mybootstrap.css">
     <link rel="stylesheet" href="css/style_cv.css">
     <link rel="stylesheet" href="css/skill.css">
-    
+
 </head>
 
 <body>
-<div class="container">
-    <form action="includes/reg_attempt.php "method="post">
-    <div class="row">
-      <h4>Information for CV</h4>
-      <div class="input-group input-group-icon">
-        <input name="name" type="text" placeholder="Full Name"/>
-        <div class="input-icon"><i class="fa fa-user"></i></div>
-      </div>
-    <div class="input-group input-group-icon">
-        <input name="name" type="text" placeholder="UIU ID"/>
-        <div class="input-icon"><i class="fa fa-file"></i></div>
-    </div> 
-        
-      <div class="input-group input-group-icon">
-        <input name="email" type="email" placeholder="Email Address"/>
-        <div class="input-icon"><i class="fa fa-envelope"></i></div>
-      </div>
-        <div class="input-group input-group-icon">
-        <input name="tel" type="tel" placeholder="Contact Number"/>
-        <div class="input-icon"><i class="fa fa-phone"></i></div>
-      </div>
-        
-      <div class="input-group input-group-icon">
-        <input name="password" type="password" placeholder="Password"/>
-        <div class="input-icon"><i class="fa fa-key"></i></div>
-      </div>
-        
-        <!-- skill sets start-->
-   
-        
-    </div>
-    <div class="row">
-      <div class="col-half">
-        <h4>Date of Birth</h4>
-        <div class="input-group">
-          <div class="col-sixth">
-            <input name="date" type="date" placeholder="MM"/>
-          </div>
-          <!--<div class="col-third">
-            <input type="text" placeholder="MM"/>
-          </div>
-          <div class="col-third">
-            <input type="text" placeholder="YYYY"/>
-          </div>-->
-        </div>
-      </div>
-      <div class="col-half">
-        <h4>Gender</h4>
-        <div class="input-group">
-          <input name="gender" type="radio" name="gender" value="male" id="gender-male"/>
-          <label for="gender-male">Male</label>
-          <input name="gender" type="radio" name="gender" value="female" id="gender-female"/>
-          <label for="gender-female">Female</label>
-        </div>
-      </div>
-    </div>
-    
-    
-     
-  </form>
-    <!--skill starts here -->
-    <div class="row"> <h4>Please select your Skill Set</h4> </div>
-    
-    <div class="row">
-       
-        <div class="col">
+    <div class="container">
+        <form action="includes/reg_attempt.php " method="post">
             <div class="row">
-        
-        <?php 
-        $arr=array("C","Java","C++","PHP","Python","Javascript");
-        for($i=0;$i<count($arr);$i++)
-        {?>
-       
-        <label class="containerr"><?php echo $arr[$i]; ?> 
-            &ensp;
-          <input type="checkbox" >
-          <span class="checkmark"></span>
-        </label>
-        
-        
-        <?php
-        }
-         
-       ?> 
-        
+                <h4>Information for CV</h4>
+                <div class="input-group input-group-icon">
+                    <input name="name" type="text" placeholder="Full Name" required/>
+                    <div class="input-icon"><i class="fa fa-user"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input name="name" type="text" placeholder="UIU ID" required/>
+                    <div class="input-icon"><i class="fa fa-file"></i></div>
+                </div>
+
+                <div class="input-group input-group-icon">
+                    <input name="email" type="email" placeholder="Email Address" required/>
+                    <div class="input-icon"><i class="fa fa-envelope"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input name="tel" type="tel" placeholder="Contact Number" />
+                    <div class="input-icon"><i class="fa fa-phone"></i></div>
+                </div>
+
+                <div class="input-group input-group-icon">
+                    <input name="password" type="password" placeholder="Password" required/>
+                    <div class="input-icon"><i class="fa fa-key"></i></div>
+                </div>
+
+                <!-- skill sets start-->
+
+
+            </div>
+            <div class="row">
+                <div class="col-half">
+                    <h4>Date of Birth</h4>
+                    <div class="input-group">
+                        <div class="col-sixth">
+                            <input name="date" type="date" placeholder="Date" required/>
+                        </div>
+                       
+                    </div>
+                </div>
+                <div class="col-half">
+                    <h4>Gender</h4>
+                    <div class="input-group">
+                        <input name="gender" type="radio" value="male" id="gender-male" />
+                        <label for="gender-male">Male</label>
+                        <input name="gender" type="radio" value="female" id="gender-female" />
+                        <label for="gender-female">Female</label>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </form>
+        <!--skill starts here -->
+        <div class="row">
+            <h4>Please select your Skill Set</h4>
         </div>
-         <div class="col-half">
-            <div class="row"></div>
+
+        <div class="row">
+
+            <div class="col">
+                <div class="row">
+
+                    <?php 
+              $arr=array("asp.net","c++","c","java","c#","go","kotlin","ruby","r","swift","php","python","javascript","laravel","angular.js","node.js","spring","android","ios");
+                    sort($arr);
+              for($i=0;$i < count($arr); $i++)
+              { ?>
+
+                    <label class="containerr" ><?php echo $arr[$i]; ?> 
+                    &ensp;
+                     <input type="checkbox" name="<?php echo $arr[$i]; ?> " >
+                    <span class="checkmark"></span>
+                    </label>
+
+
+                    <?php
+             }
+         
+                    ?>
+
+                </div>
+                <div class="col-half">
+                    <div class="row"></div>
+
+                </div>
+
+                   
+
+            </div>
+            
+            
+            
+            <div class="row">
+               
+                
+
+            </div>
 
         </div>
+
+
+        <!--skill ends here -->
         
-          
-  
-       </div>
-    <div class="row">
+        <div class="row">
+        <h4>Achievements/ECC</h4>
+                    <div class="input-group input-group-icon">
+                        <textarea placeholder="e.g. programming contests, hackathons etc..." rows="5" cols="60"></textarea>
+                    </div>
+        </div>
         
-     &emsp;   <h4>Terms and conditions</h4>
+        <div class="row">
+        <h4>Terms and conditions</h4>
+            <div class="row">
+
+                    <input class="col" type="checkbox" id="terms" required>
+                    <label for="terms" style="float:left;"><h7>I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</h7></label>
+                </div>
+        </div>
         
-        
-    <div class="row">
-       
-        <input class="clm" type="checkbox" id="terms" required>
-        <label for="terms"   style="float:left;" ><h7>I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</h7></label>
-      </div>
-    
-    <div class="row">
-        &emsp;
-        <a><button 
-                 style="color:white;
+        <div class="row">
                     
+                    <a><button 
+                 style="color:white;
+                    padding-left: 5px;
                     font-family: sans-serif;
                     border-radius: 2px;
                     background-color: orange;
                     border-color: whitesmoke;"
-    type="submit" >Submit</button></a> 
+                type="submit" >Submit</button></a>
+                </div>
+         
+
+
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="vendor/bootstrap/js/index.js"></script>
+
+
     </div>
-      
-    </div>
-    
-    </div>
-    
-    
-    
- 
-      
-  
-
-    
-    <!--skill ends here -->
-    
-
-
-    
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-<script  src="vendor/bootstrap/js/index.js"></script>
- 
-
-
 
 </body>
-    
-    
+
+
 </html>
--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="navbar">
-    
-         <img src="CDIP-Logo.png">
-  
-   
+
+    <img src="CDIP-Logo.png">
+
+
     <div class="topnav-right">
-     <!--   <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+        <!--   <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
         <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
         <a href="#"><i class="fa fa-fw fa-user"></i> Login<br></a> -->
         <a href="#about">Home</a>
         <a href="/python/cv_input.php">CV</a>
         <a href="#about">Login</a>
-     
+
     </div>
 </div>
 
 <div class="main">
-  <p> hello</p>
+    <p> hello</p>
 </div>
 <?php
 //running demo scrapper to csv file
@@ -35,24 +35,26 @@
         }
         ///this is where html begins
        ?>
-        <div align="center">
-            <div class="card" align="left">
-              <link rel="stylesheet" href="style.css">
-              <div class="container">
-                  <h4><b><?php echo $line[0];?></b></h4> 
-                   <p><?php echo $line[1];?></p> 
-                    <a href="<?php echo $line[2];?>"> <button class="btn-link">Read more</button></a>
+    <div align="center">
+        <div class="card" align="left">
+            <link rel="stylesheet" href="style.css">
+            <div class="container">
+                <h4><b><?php echo $line[0];?></b></h4>
+                <p>
+                    <?php echo $line[1];?>
+                </p>
+                <a href="<?php echo $line[2];?>"> <button class="btn-link">Read more</button></a>
 
-              </div>
             </div>
-            
-            <div>
-            
-            </div>
+        </div>
+
+        <div>
 
         </div>
-        
-       <?php
+
+    </div>
+
+    <?php
         ///html ends here
       } 
    
